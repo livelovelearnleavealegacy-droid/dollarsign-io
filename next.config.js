4777882cba2976cfff11d765173365b2c2b321f4
livelovelearnleavealegacy-droid <livelovelearnleavealegacy@gmail.com>
2026-09-09 06:49:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.dollarsign.io" }],
+        destination: "https://dollarsign.io/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
